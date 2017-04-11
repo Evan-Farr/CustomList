040197f1757191cfefd4ce291fba1e87d13116ee
Evan-Farr <evan.c.farr@gmail.com>
2017-04-11 14:01:49 -0500

    added object class

--- CustomListProject/CustomListTests/UnitTest1.cs
+++ CustomListProject/CustomListTests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace CustomListTests
     public class UnitTest1
     {
         [TestMethod]
-        public void CustomList_AddOneInt() //can I add one int into the list, and is it at spot 0?
+        public void AddOneInt() //can I add one int into the list, and is it at spot 0?
         {
             //Arrange
             CustomList<int> list = new CustomList<int>();
@@ -20,7 +20,19 @@ namespace CustomListTests
         }
 
         [TestMethod]
-        public void CustomList_AddOneInt_Initializer() //can I add one int into the list using initializer?
+        public void AddOneInt2() //can I add one int into already populated list, and is it at last index?
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>() { 1, 2, 3, 4 };
+            int number = 5;
+            //Act
+            list.Add(number);
+            //Assert
+            Assert.AreEqual(number, list[4]);
+        }
+
+        [TestMethod]
+        public void AddOneInt_Initializer() //can I add one int into the list using initializer?
         {
             //Arrange
             int number = 1;
@@ -31,7 +43,7 @@ namespace CustomListTests
         }
 
         [TestMethod]
-        public void CustomList_AddIntToIndexTwo_Initializer() //can I add two ints into the list, and each be in their own spot in order I pass them in using initializer?
+        public void AddIntToIndexTwo_Initializer() //can I add two ints into the list, and each be in their own spot in order I pass them in using initializer?
         {
             //Arrange
             int number1 = 1;
@@ -43,7 +55,7 @@ namespace CustomListTests
         }
 
         [TestMethod]
-        public void CustomList_AddOneIntAndOneString() //can I add a string into a list of int?
+        public void IntToString() //can I convert int to string?
         {
             //Arrange
             CustomList<int> list = new CustomList<int>();
