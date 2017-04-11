083b6ed76348d7c8e368421765c29fa41e690beb
Evan-Farr <evan.c.farr@gmail.com>
2017-04-11 12:31:24 -0500

    heading to lunch

--- CustomListProject/CustomListTests/UnitTest1.cs
+++ CustomListProject/CustomListTests/UnitTest1.cs
@@ -8,13 +8,48 @@ namespace CustomListTests
     public class UnitTest1
     {
         [TestMethod]
-        public void CustomList_AddInto_Int()
+        public void CustomList_AddOneInt() //can I add one int into the list, and is it at spot 0?
         {
             //Arrange
             CustomList<int> list = new CustomList<int>();
             int number = 1;
             //Act
-            list.AddInto(number);
+            list.Add(number);
+            //Assert
+            Assert.AreEqual(number, list[0]);
+        }
+
+        [TestMethod]
+        public void CustomList_AddOneInt_Initializer() //can I add one int into the list using initializer?
+        {
+            //Arrange
+            int number = 1;
+            //Act
+            CustomList<int> list = new CustomList<int>() { number };
+            //Assert
+            Assert.AreEqual(number, list[0]);
+        }
+
+        [TestMethod]
+        public void CustomList_AddIntToIndexTwo_Initializer() //can I add two ints into the list, and each be in their own spot in order I pass them in using initializer?
+        {
+            //Arrange
+            int number1 = 1;
+            int number2 = 2;
+            //Act
+            CustomList<int> list = new CustomList<int>() { number1, number2 };
+            //Assert
+            Assert.AreEqual(number2, list[1]);
+        }
+
+        [TestMethod]
+        public void CustomList_AddOneIntAndOneString() //can I add a string into a list of int?
+        {
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int number = 1;
+            //Act
+            list.Add(number);
             //Assert
             Assert.AreEqual(number, list[0]);
         }
