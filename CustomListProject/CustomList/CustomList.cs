bb6af6476bd28a56a0157e95ddc55a3ce5470721
Evan-Farr <evan.c.farr@gmail.com>
2017-04-12 11:36:40 -0500

    working on add method

--- CustomListProject/CustomList/CustomList.cs
+++ CustomListProject/CustomList/CustomList.cs
@@ -8,6 +8,32 @@ namespace CustomList
 {
     public class CustomList<T>
     {
-       
+        private int count;
+        private int capacity;
+        public T[] list;
+
+        public int Count { get { return count; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+
+        public CustomList()
+        {
+            count = 0;
+            capacity = 10;
+            list = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            if(count < capacity)
+            {
+
+                count += 1;
+            }
+            else
+            {
+                list = new T[capacity + 1];
+                
+            }
+        }
     }
 }
